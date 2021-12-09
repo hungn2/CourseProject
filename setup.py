54acbb2dae3ef5052ea298cca28a3ef1f0638b62
Blake Jones <bjones325@gatech.edu>
2021-12-08 19:44:07 -0500

    Force dependencies install

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 
+requirementPath = 'requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+
 setup(name='slackwire',
       version='1.0',
       description='Query Slack and Campuswire for relevant topics',
@@ -7,6 +14,7 @@ setup(name='slackwire',
       requirements='requirements.txt',
       packages=find_packages(),
       include_package_data=True,
+      install_requires=install_requires,
       package_data={'': ['datasets/*','datasets/campuswire/*','datasets/slack/*','datasets/combined/*']},
       entry_points={
           'console_scripts': [
